cd9a8e094af5f15ba4d58d6450664b2d539bcb62
Jean-rené Decoster <jeanrene.decoster@gmail.com>
2023-02-09 12:23:25 +0100

    Connect_frontWithGQLAI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import "./App.css";
+
 
 function App() {
+function Create1User() {
+  const Create_Users = gql`
+    mutation Mutation($data: UserInput!) {
+      createUser(data: $data) {
+        email
+        id
+      }
+    }
+  `;
+  const { data } = useQuery(Create_Users);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
