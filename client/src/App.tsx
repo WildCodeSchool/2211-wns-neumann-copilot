5a7ef121035fbd20c04cc16ee92060bef806dc8c
Jean-rené Decoster <jeanrene.decoster@gmail.com>
2023-02-10 14:40:51 +0100

    react router added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import "./App.css";
 import { useGetUsersQuery } from "./gql/generated/schema";
+import { Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
 
 function App() {
   const { data } = useGetUsersQuery();
   const users = data?.getUsers || [];
   console.log(users);
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <p>
-        {users.map((user) => {
-          return user.id;
-        })}
-      </p>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header"></header>
+        <p>
+          {users.map((user) => {
+            return user.id;
+          })}
+        </p>
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 export default App;
