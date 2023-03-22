a30ed6afd74eac9e602cae1747b4fb3a54684641
unknown <florian.cagnon@live.fr>
2023-03-22 17:03:57 +0100

    ajout du header, footer et de la home page avec les styles mobile sans responsive et sans liens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-import "./App.css";
-import { useGetUsersQuery } from "./gql/generated/schema";
 import { Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
+import "./App.css";
+import { useGetUsersQuery } from "./gql/generated/schema";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import ImageDeCovoiturage from "./img/ImageDeCovoiturage.svg"
 
 function App() {
   const { data } = useGetUsersQuery();
   const users = data?.getUsers || [];
   console.log(users);
   return (
-    <>
-      <div className="App">
-        <header className="App-header"></header>
+    <div className="App">
+      <Header></Header>
+      <div className="app">
+        <h1>Bienvenue chères Copilote !</h1>
+        <img src={ImageDeCovoiturage} alt="véhicule avec quatre personnes à l'intérieur" />
+        <h2>Recherchez, cliquez et covoiturez !</h2>
+        <p>Trouver un trajet devient encore plus simple ! Facile d'utilisation et dotée de technologies avancées, notre appli vous permet de trouver un trajet à proximité en un rien de temps.</p>
+        <button>Trajet</button>
+      </div>
+      <Footer></Footer>
+      {/* <header className="App-header"></header>
         <p>
           {users.map((user) => {
             return user.id;
@@ -19,9 +30,8 @@ function App() {
         </p>
         <Routes>
           <Route path="/Login" element={<Login />} />
-        </Routes>
-      </div>
-    </>
+        </Routes> */}
+    </div>
   );
 }
 export default App;
---
+++ client/src/Components/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Copyright 2023 by J-R, Rémy et Florian</p>
+        </div>
+    );
+}
+export default Footer;
