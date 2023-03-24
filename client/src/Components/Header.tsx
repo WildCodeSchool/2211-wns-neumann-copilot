a10663bb752e2a15d0e3ea3b6d1a94abee6a75bb
unknown <florian.cagnon@live.fr>
2023-03-24 14:29:38 +0100

    ajout d'une redirection sur le logo de profil du Headerselon si l'utilisateur est connecté ou non

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -3,15 +3,22 @@ import LogoCopilote from "../img/LogoCopilote.svg";
 import BarreMenu from "../img/BarreMenu.png";
 import StandardProfil from "../img/StandardProfil.svg";
 import { Link } from "react-router-dom";
+import { useGetProfileQuery } from "../gql/generated/schema";
 
 function Header() {
+    
+    const { data: currentUser, client } = useGetProfileQuery({
+        errorPolicy: "ignore",
+    });
+    console.log(currentUser);
+
     return (
         <div className="header">
             <img src={BarreMenu} alt="logo de Copilote" className="menu" />
             <Link to={"/"}>
                 <img src={LogoCopilote} alt="logo de Copilote" className="logo" />
             </Link>
-            <Link to={"/login"}>
+            <Link to={currentUser?.profile ? "/Profil" : "/login"}>
                 <img src={StandardProfil} alt="logo de Copilote" className="profil-img" />
             </Link>
         </div>
