2a19546505c39a6748a2b2056d2064cd5bb00ff3
unknown <florian.cagnon@live.fr>
2023-03-24 11:52:25 +0100

    inscription d'un utilisateur

--- client/src/screens/Inscription.tsx
+++ client/src/screens/Inscription.tsx
@@ -5,31 +5,76 @@ import "./css/Inscription.css"
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import LeftMenuPC from "../Components/LeftMenuPC";
+import { useCreateUserMutation, useGetProfileQuery } from "../gql/generated/schema";
+import { useState } from "react";
 
-export default function Loginn() {
+export default function Inscription() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const [create_user] = useCreateUserMutation();
+
+    const { data: currentUser, client } = useGetProfileQuery({
+        errorPolicy: "ignore",
+    });
+    const isAdmin = currentUser?.profile.role === "admin";
+    console.log(currentUser);
+
     return (
         <div>
-        {window.innerWidth < 992 ? <Header></Header> : <LeftMenuPC></LeftMenuPC>}
-        <div className="main">
-            <h1>Inscription</h1>
+            {window.innerWidth < 992 ? <Header></Header> : <LeftMenuPC></LeftMenuPC>}
+            <div className="main">
+                <h1>Inscription</h1>
 
-            <div className="container_form">
-                <form className="form">
-                    <div className="input_inscription">
-                        <input type="email" name="email" placeholder="Votre adresse email"/>
-                    </div>
-                    <div className="input_inscription">
-                        <input type="password" name="password" placeholder="Votre mot de passe"/>
-                    </div>
-                    <div className="redirection_sign_in">
-                <p>Déjà un compte ?</p>&nbsp;<Link to="/login">Connexion</Link>
+                <div className="container_form">
+                    <form
+                        className="form"
+                        onSubmit={async (e) => {
+                            e.preventDefault();
+                            setError("");
+                            try {
+                                await create_user({ variables: { data: { email, password } } });
+                            } catch (err) {
+                                console.error(err);
+                                setError("invalid credentials");
+                            } finally {
+                                client.resetStore();
+                            }
+                        }}
+                    >
+                        <div className="input_inscription">
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Votre adresse email"
+                            />
+                        </div>
+                        <div className="input_inscription">
+                            <input
+                                type="text"
+                                id="password"
+                                name="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Votre mot de passe"
+                            />
+                        </div>
+                        {error && <p>{error}</p>}
+                        <div className="redirection_sign_in">
+                            <p>Déjà un compte ?</p>&nbsp;<Link to="/login">Connexion</Link>
+                        </div>
+                        <div>
+                            <button className="button button_inscription">Inscription</button>
+                        </div>
+                    </form>
                 </div>
-                    <div>
-                        <button className="button button_inscription">Connexion</button>
-                    </div>
-                </form>
             </div>
+            {window.innerWidth < 992 ? <Footer></Footer> : ''}
+            <div> {isAdmin && <p>C'est le boss</p>}</div>
         </div>
-        {window.innerWidth < 992 ? <Footer></Footer> : ''}
-    </div>
-)}
+    )
+}
