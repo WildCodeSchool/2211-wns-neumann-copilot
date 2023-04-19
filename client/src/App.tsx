daa08cc8fc6383cbf74d12f09a00cee2ffad1d17
J-R <jrd@weareled.fr>
2023-04-19 16:17:49 +0200

    playwright work in progress + add post json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
-<<<<<<< HEAD
-import SignUp from "./screens/SignUp";
-=======
 import Register from "./screens/Register";
->>>>>>> develop
 import Profil from "./screens/Profil";
 import "./App.css";
 import { useGetUsersQuery } from "./gql/generated/schema";
@@ -14,25 +10,15 @@ import Trajet from "./screens/Trajet";
 function App() {
   const { data } = useGetUsersQuery();
   const users = data?.getUsers || [];
-  console.log(users);
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/trajet" element={<Trajet />} />
-<<<<<<< HEAD
-        <Route path="/signUp" element={<SignUp />} />
-=======
         <Route path="/register" element={<Register />} />
->>>>>>> develop
         <Route path="/Profil" element={<Profil />} />
       </Routes>
-      {/* <p>
-          {users.map((user) => {
-            return user.id;
-          })}
-        </p> */}
     </div>
   );
 }
