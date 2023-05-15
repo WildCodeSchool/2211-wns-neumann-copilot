87c8e49ff2d146f576ce8d114f72de4907948fba
Florian59c <florian.cagnon@live.fr>
2023-05-15 16:24:30 +0200

    creation de la route

--- native-client/App.tsx
+++ native-client/App.tsx
@@ -3,14 +3,53 @@ import { StyleSheet, Text, View } from 'react-native';
 import Trajet from "./screens/Trajet";
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from './gql/apolloClient';
+import { NavigationContainer } from '@react-navigation/native'; import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TrajetsListe from './screens/TrajetsListe';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <View>
-        <Trajet />
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              if (route.name === "Trajet") {
+                return (
+                  <Ionicons
+                    name={focused ? "person-circle" : "person-circle-outline"}
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "TrajetsListe") {
+                return (
+                  <Ionicons
+                    name={focused ? "people-circle" : "people-circle-outline"}
+                    size={size}
+                    color={color}
+                  />
+                );
+              }
+              return (
+                <Ionicons
+                  name={"alert-circle"}
+                  size={size}
+                  color={color}
+                />
+              );
+            },
+            tabBarActiveTintColor: "green",
+            tabBarInactiveTintColor: "gray",
+            tabBarStyle: { height: 60, paddingBottom: 10 }
+          })}
+        >
+          <Tab.Screen name="Trajet" component={Trajet} />
+          <Tab.Screen name="TrajetsListe" component={TrajetsListe} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
 
   );
---
+++ native-client/screens/TrajetsListe.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function TrajetsListe() {
+    return <Text>page trajets liste</Text>
+}
