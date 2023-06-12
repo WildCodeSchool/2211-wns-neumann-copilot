ade8f8949c583c480601e854e48f47c9e51942ff
J-R <jrd@weareled.fr>
2023-06-12 16:50:15 +0200

    work in progress

---
+++ client/src/Components/CarpoolList.tsx
@@ -0,0 +1,28 @@
+import { CarPool, useGetCarPoolsQuery } from "../gql/generated/schema";
+
+export default function CarpoolList() {
+  const { data } = useGetCarPoolsQuery();
+  const carPoolsList = data?.getCarPools;
+  console.log(carPoolsList);
+  return (
+    <div>
+      {!carPoolsList.length ? (
+        <p>Aucun covoiturage trouvé</p>
+      ) : (
+        carPoolsList.map((carPool: CarPool) => {
+          <div key={carPool.id} className="carPoolCard">
+            <div className="carPoolCard-departure">
+              <p> Ville de depart : {carPool.departureCity}</p>
+            </div>
+            <div className="carPoolCard-arrival">
+              <p> Ville d'arrivée : {carPool.arrivalCity}</p>
+            </div>
+            <div className="carPoolCard-time">
+              <p> Ville d'arrivée : {carPool.departureDateTime}</p>
+            </div>
+          </div>;
+        })
+      )}
+    </div>
+  );
+}
--- client/src/screens/Trajet.tsx
+++ client/src/screens/Trajet.tsx
@@ -6,6 +6,7 @@ import {
 } from "../gql/generated/schema";
 import "./css/Trajet.css";
 import { useEffect, useState } from "react";
+import CarpoolList from "../Components/CarpoolList";
 
 function Trajet() {
   const { data: currentUser, client } = useGetProfileQuery({
@@ -19,6 +20,7 @@ function Trajet() {
   const [passengerNumber, setPassengerNumber] = useState("");
   const [error, setError] = useState("");
   const [driverId, setDriverId] = useState(0);
+  const [toggle, setToggle] = useState(false);
   const passengerId = "";
 
   useEffect(() => {
@@ -26,92 +28,98 @@ function Trajet() {
       setDriverId(currentUser.profile.id);
     }
   }, [currentUser]);
-
+  // console.log(toggle);
+  function handleToggle() {
+    setToggle(!toggle);
+  }
   return (
     <div className="trajet">
       <div className="app">
         <div className="toggle">
-          <p>je recherche</p>
+          <p>je propose</p>
           <label className="switch">
-            <input type="checkbox" />
+            <input type="checkbox" id="toggle" onChange={handleToggle} />
             <span className="slider round"></span>
           </label>
-          <p>je propose</p>
+          <p>je recherche</p>
         </div>
-
-        <form
-          className="form_create_carpool"
-          onSubmit={async (e) => {
-            e.preventDefault();
-            setError("");
-            try {
-              await createCarPool({
-                variables: {
-                  data: {
-                    departureCity,
-                    arrivalCity,
-                    departureDateTime,
-                    passengerNumber,
-                    driverId,
-                    passengerId,
+        {toggle ? (
+          <CarpoolList />
+        ) : (
+          <form
+            className="form_create_carpool"
+            onSubmit={async (e) => {
+              e.preventDefault();
+              setError("");
+              try {
+                await createCarPool({
+                  variables: {
+                    data: {
+                      departureCity,
+                      arrivalCity,
+                      departureDateTime,
+                      passengerNumber,
+                      driverId,
+                      passengerId,
+                    },
                   },
-                },
-              });
-            } catch (err) {
-              console.error(err);
-              setError("invalid credentials");
-            } finally {
-              client.resetStore();
-              navigate("/trajet");
-            }
-          }}
-        >
-          {/* Ville de Départ */}
-          <div className="input">
+                });
+              } catch (err) {
+                console.error(err);
+                setError("invalid credentials");
+              } finally {
+                client.resetStore();
+                navigate("/trajet");
+              }
+            }}
+          >
+            {/* Ville de Départ */}
+            <div className="input">
+              <input
+                type="text"
+                placeholder="Ville de départ"
+                name=""
+                onChange={(e) => setDepartureCity(e.target.value)}
+                value={departureCity}
+              />
+            </div>
+            {/* Ville d'arrivée */}
+            <div className="input">
+              <input
+                type="text"
+                placeholder="Ville d'arrivée"
+                name=""
+                onChange={(e) => setArrivalCity(e.target.value)}
+                value={arrivalCity}
+              />
+            </div>
+            {/* Jour et heure du départ */}
+            <div className="input">
+              <input
+                type="datetime-local"
+                placeholder=""
+                name=""
+                onChange={(e) => setDepartureDateTime(e.target.value)}
+                value={departureDateTime}
+              />
+            </div>
+            {/* Nombre de passager */}
+            <div className="input_carpool">
+              <input
+                type="text"
+                placeholder="Nombre de passager"
+                name=""
+                onChange={(e) => setPassengerNumber(e.target.value)}
+                value={passengerNumber}
+              />
+            </div>
             <input
-              type="text"
-              placeholder="Ville de départ"
-              name=""
-              onChange={(e) => setDepartureCity(e.target.value)}
-              value={departureCity}
+              type="submit"
+              className="button button_validate"
+              value="Valider"
             />
-          </div>
-          {/* Ville d'arrivée */}
-          <div className="input">
-            <input
-              type="text"
-              placeholder="Ville d'arrivée"
-              name=""
-              onChange={(e) => setArrivalCity(e.target.value)}
-              value={arrivalCity}
-            />
-          </div>
-          {/* Jour et heure du départ */}
-          <div className="input">
-            <input
-              type="datetime-local"
-              placeholder=""
-              name=""
-              onChange={(e) => setDepartureDateTime(e.target.value)}
-              value={departureDateTime}
-            />
-          </div>
-          {/* Nombre de passager */}
-          <div className="input_carpool">
-            <input
-              type="text"
-              placeholder="Nombre de passager"
-              name=""
-              onChange={(e) => setPassengerNumber(e.target.value)}
-              value={passengerNumber}
-            />
-          </div>
-          <input
-            type="submit"
-            className="button button_validate"
-            value="Valider"
-          />
-        </form>
+          </form>
+        )}
       </div>
     </div>
   );
